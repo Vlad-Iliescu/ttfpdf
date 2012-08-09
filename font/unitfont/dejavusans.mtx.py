ec140f02297085e58629dfc58192dc38de0db67e
Vlad.Iliescu.88@gmail.com <Vlad.Iliescu.88@gmail.com@902d357e-2ca8-7ca2-315e-a47cfe1279cf>
2012-08-09 13:12:39 +0000

    First working version

--- font/unitfont/dejavusans.mtx.py
+++ font/unitfont/dejavusans.mtx.py
@@ -3,6 +3,6 @@ type = "TTF"
 desc = {'MissingWidth': 600, 'StemV': 87, 'Flags': 4, 'Descent': 31764, 'FontBBox': '[30979 31584 1680, 1166]', 'CapHeight': 928, 'ItalicAngle': 0, 'Ascent': 928}
 up = -63
 ut = 43
-ttffile = "E:\Dev\pyfpdf\font\unitfont\DejaVuSans.ttf"
+ttffile = "E:\Dev\ttfpdf\font\unitfont\DejaVuSans.ttf"
 originalsize = 720012
 fontkey = "dejavu"
